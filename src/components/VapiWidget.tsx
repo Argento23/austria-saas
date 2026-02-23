9e92eeeeaf276aa449a9d24fa2cb33dc268ba216
Argento23 <gustavodornhofer@gmail.com>
2026-02-23 17:03:26 -0300

    fix: apply pub- prefix to vapi api key

--- src/components/VapiWidget.tsx
+++ src/components/VapiWidget.tsx
@@ -12,7 +12,7 @@ export default function VapiWidget() {
                 if (window.vapiSDK) {
                     // @ts-ignore
                     window.vapiSDK.run({
-                        apiKey: "1858a4ab-1eb6-4ca3-99df-d1da31150892",
+                        apiKey: "pub-1858a4ab-1eb6-4ca3-99df-d1da31150892",
                         assistant: "c500ab0f-ea72-47fd-b66f-e2caf54a0ebf", // Stefan New
                         config: {
                             position: "bottom-right",
